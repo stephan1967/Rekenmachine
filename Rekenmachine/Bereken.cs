b938b3f77a841efb9c0a63862fe05e7d8e61e708
Stephan <Stephan@LAPTOP-STEPHAN>
2019-02-24 16:40:30 +0100

    Add project files.

---
+++ Rekenmachine/Bereken.cs
@@ -0,0 +1,45 @@
+﻿namespace Rekenmachine
+{
+
+    /*
+      The main Math class
+      Contains all methods for performing basic math functions
+    */
+    /// <summary>
+    /// The main Math class.
+    /// Contains all methods for performing basic math functions.
+    /// </summary>
+
+
+    public class Bereken
+    {
+
+        // Adds two integers and returns the result
+        /// <summary>
+        /// Adds two integers and returns the result.
+        /// </summary>
+        public int Optellen (int a, int b)
+        /// <summary>
+        /// Met deze methode kun je 2 getallen bij elkaar optellen
+        /// </summary>
+        { return a + b; }
+
+
+        public int Aftrekken(int a, int b)
+        /// <summary>
+        /// Met deze methode kun je 2 getallen van elkaar aftrekken
+        /// </summary>
+        { return a - b; }
+
+
+        public int Delen(int a, int b)
+        /// <summary>
+        /// Met deze methode kun je 2 getallen door elkaar delen
+        /// </summary>
+        { return a / b; }
+
+
+        public int Vermenigvuldigen(int a, int b)   { return a * b; }
+
+    }
+}
---
+++ Rekenmachine/readme.txt
@@ -0,0 +1,18 @@
+This calculator is DA best calculator in Da world cause is was created by Da CyberStef.
+All credits go to him, we love him a lot. He is the best.
+------------------------------------------------------------------------
+
+
+
+
+
+
+
+Hail to da STEF 
+
+
+
+
+
+
+------------------------------------------------------------------------
